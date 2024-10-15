6adfb5d1cd7e11ce3cc249093361c32ce095f7f0
Rodrigo-Sebastian <rodrigo.seb93@gmail.com>
2024-10-15 23:15:06 +0200

    uppdaterat api URL för att kunna använda https

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const API_KEY = '9e9b0f23';
 
 export const fetchMovies = async(title) => {
-        const response = await axios.get(`http://www.omdbapi.com/?s=${title}&apikey=${API_KEY}`);
+        const response = await axios.get(`https://www.omdbapi.com/?s=${title}&apikey=${API_KEY}`);
         return response.data.Search;
     
 };
 
 export const fetchMovieDetails = async (id) => {
-    const response = await axios.get(`http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`);
+    const response = await axios.get(`https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`);
     return response.data;
 }
